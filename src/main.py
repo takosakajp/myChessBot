23c48028505f91c5ec87b69fae65cf742b428b5c
to95 <to95@cornell.edu>
2024-12-29 23:42:58 -0500

    Initial board setup

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+from const import *
+from game import Game
+
+
+class Main():
+    
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+        pygame.display.set_caption("My Chess Bot")
+        self.game = Game()
+    
+    def mainloop(self):
+        self.game.show_bg(self.screen)
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+    
+
+main = Main()
+main.mainloop()
